b566045d8b4dd88a1a566d47b3c0979028f366aa
Henrileregis <henrileregis@alu.ufc.br>
2021-05-18 21:26:16 -0300

    Create primeirotrabalho.java

---
+++ primeirotrabalho.java
@@ -0,0 +1,18 @@
+/**
+ * Primeiro trabalho da disciplina.
+ * 
+ * @author (Henrileregis) 
+ * @version (1.0 18/05/2021)
+ */
+public class Helloword
+{
+  private String saudacao;
+  
+      public Helloword (String n){
+          saudacao = n;
+          
+          System.out.print(saudacao);
+      }
+
+  
+}
